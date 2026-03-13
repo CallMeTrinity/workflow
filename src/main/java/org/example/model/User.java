06c837ec967f5ea6910240374f9b640a8a2843c1
CallMeTrinity <anteur.ap@gmail.com>
2026-03-13 09:19:28 +0100

    feat : add user and enums

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,87 @@
+package org.example.model;
+
+import org.example.model.enums.Role;
+
+// Représente un utilisateur de l'application
+
+public class User {
+
+    private Long id;
+    private String lastName;
+    private String firstName;
+    private String mail;
+    private String password;
+    private Role role;
+
+    public User() {
+    }
+
+    // Constructeur principal
+
+    public User(Long id, String lastName, String firstName, String mail, String password, Role role) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.mail = mail;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getter et Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    // Retourne le nom complet de l'utilisateur
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
+
+
