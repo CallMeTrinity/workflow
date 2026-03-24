6f48d238a2d4385fa09f52851cf9e3df1d15c42e
anton <anteur.ap@gmail.com>
2026-03-24 12:37:10 +0100

    feat : redirection after connection

--- src/main/java/org/example/ui/controller/LoginController.java
+++ src/main/java/org/example/ui/controller/LoginController.java
@@ -1,13 +1,18 @@
 package org.example.ui.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.example.exception.InvalidLoginCredentialsException;
 import org.example.exception.UserNotFoundException;
 import org.example.model.User;
 import org.example.service.AuthService;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -30,8 +35,18 @@ public class LoginController {
         try {
             User user = authService.login(email, password);
             System.out.println("Logged in as : " + user.getFullName());
-            // @TODO naviguer vers le dashboard
-        } catch (UserNotFoundException | InvalidLoginCredentialsException e){
+
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/dashboard.fxml"));
+                Scene scene = new Scene(loader.load(), 800, 600);
+                Stage stage = (Stage) emailField.getScene().getWindow();
+                stage.setScene(scene);
+                stage.setTitle("Workflow - Dashboard");
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to load dashboard", e);
+            }
+
+        } catch (UserNotFoundException | InvalidLoginCredentialsException e) {
             errorLabel.setText(e.getMessage());
         }
     }
