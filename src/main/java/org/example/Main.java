6a9e3624b535e172b9a04f950bb95841d733267d
anton <anteur.ap@gmail.com>
2026-03-27 13:26:31 +0100

    feat: enhance UI with updated styles and splash screen integration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,14 +11,20 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        // Icône de la fenêtre / taskbar
+        stage.getIcons().add(
+                new Image(getClass().getResourceAsStream("/images/workflow_logo.png"))
+        );
+
+        // Démarre sur le splash screen
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/splash.fxml"));
         Scene scene = new Scene(loader.load());
 
         scene.getStylesheets().add(
                 getClass().getResource("/css/app.css").toExternalForm()
         );
 
-        stage.setTitle("ProjectRoom");
+        stage.setTitle("WorkFlow");
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show();
